2e0693fa4b018e22966c64780395bbc8d12f0441
titouanmoreau <titouan.moreau@students.plymouth.ac.uk>
2021-11-09 15:10:49 +0000

    Update main.cpp

--- Tasks/Task-132-SwitchCase/main.cpp
+++ Tasks/Task-132-SwitchCase/main.cpp
@@ -34,28 +34,50 @@ int main()
             //Nothing pressed
             greenLED = 0;
             yellowLED = 0;
-            redLED = !redLED;
+            redLED = 0 ; 
             break;
 
             case 1:
             //Button A only
-            redLED = 0;
+            redLED = !redLED;
             yellowLED = 0;            
-            greenLED = !greenLED;
+            greenLED = 0;
             break;
 
-            case 3: 
-            //Button A and B
+            case 2:
+            //Button B only
             redLED = 0;
             yellowLED = !yellowLED;            
             greenLED = 0;
             break;
 
+            case 4: 
+            //Button C only
+            redLED = 0;
+            yellowLED = 0;           
+            greenLED = !greenLED;
+            break;
+
+            case 8: 
+            case 9: 
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+           
+            //Button D at all 
+            redLED = !greenLED;
+            yellowLED = !yellowLED;           
+            greenLED = !greenLED;
+            break;
+
             default:
             //All others
-            greenLED = 0;
-            yellowLED = 0;
-            redLED = 0;
+            greenLED = 1; 
+            yellowLED = 1;
+            redLED = 1;
             break;
 
         }
